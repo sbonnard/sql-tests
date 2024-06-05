1913ded226961e438f19b9ac347b732cb1b4f98e
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-05 11:23:52 +0200

    first commit, TABLES created

--- shop.sql
+++ shop.sql
@@ -16,3 +16,21 @@ CREATE TABLE orders (
     PRIMARY KEY (id_order),
     FOREIGN KEY (id_customer) REFERENCES customer(id_customer)
 );
+
+CREATE TABLE product (
+    ref_product SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name_product VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) UNSIGNED NOT NULL,
+    description VARCHAR(255),
+    PRIMARY KEY (ref_product)
+);
+
+CREATE TABLE order_product (
+    ref_product SMALLINT UNSIGNED NOT NULL,
+    id_order SMALLINT UNSIGNED NOT NULL,
+    quantity INT UNSIGNED NOT NULL,
+    price_purchase DECIMAL(10,2) UNSIGNED NOT NULL,
+    PRIMARY KEY (ref_product, id_order),
+    FOREIGN KEY (ref_product) REFERENCES product(ref_product),
+    FOREIGN KEY (id_order) REFERENCES orders(id_order)
+);
