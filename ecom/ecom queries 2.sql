55323bbd59a183478eef7830d44dfd0f085dd413
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-07 16:13:00 +0200

    Exercices 9 done

--- ecom/ecom queries 2.sql
+++ ecom/ecom queries 2.sql
@@ -50,7 +50,10 @@ GROUP BY create_by_year;
 
 -- 9/ Récupérer le montant total de la commande numéro 12
 
-
+SELECT SUM(price_order * quantity) AS total_price
+FROM product_order
+WHERE id_order = 12
+GROUP BY id_order;
 
 -- 10/ Récupérer les identifiants des commandes et pour chacune le montant total payé par le client
 
