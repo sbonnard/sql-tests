fd3d226dd5b3233b1d9bd29787b944ef16b0947b
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-10 15:29:36 +0200

    question 8 done

--- ecom/ecom queries 3.sql
+++ ecom/ecom queries 3.sql
@@ -79,7 +79,18 @@ HAVING total_quantity >= 10;
 
 -- 8/ Récupérer le nom et le total de chiffre d'affaire de tous les produits (0.00 si le produit n'a pas été vendu)
 
+SELECT ref_product, name_product, SUM(price_order * quantity) AS total_price
+FROM product p
+JOIN product_order po USING(ref_product)
+GROUP BY ref_product;
 
+-- SELECT ref_product, name_product
+-- FROM product p
+-- WHERE p.ref_product
+-- NOT IN (
+--     SELECT ref_product
+--     FROM product_order
+-- );
 
 -- 9/ Récupérer les noms des produits qui n'ont jamais été vendus
 -- à un prix aussi bas qu'aujourd'hui
