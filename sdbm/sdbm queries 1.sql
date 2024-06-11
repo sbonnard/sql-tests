8b1a854ea54bdf14102e431e3de3927a6c7c151f
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-11 14:27:52 +0200

    Ex 4 done

--- sdbm/sdbm queries 1.sql
+++ sdbm/sdbm queries 1.sql
@@ -18,13 +18,16 @@ ORDER BY purchase_price DESC LIMIT 3;
 
 SELECT continent_name, COUNT(id_country) AS all_countries_from
 FROM continent C
-JOIN country co USING(id_continent)
+    JOIN country co USING(id_continent)
 GROUP BY id_continent
 ORDER BY all_countries_from DESC LIMIT 1;
 
 -- 4 ● Quel est le nom du pays d'origine de la marque de bière "Heineken" ?
 
-
+SELECT brand_name, country_name
+FROM brand b
+    JOIN country USING (id_country)
+WHERE brand_name = 'Heineken';
 
 -- 5 ● Combien de bières ont été vendues lors de chaque transaction ? Afficher les numéros de
 -- ticket, la date de ticket, et le nombre de bières.
