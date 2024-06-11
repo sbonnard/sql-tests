42c04ef07700fba1640c2c2f02fefb3cd432239f
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-11 16:23:02 +0200

    Exercice 1 done

---
+++ sdbm/sdbm queries 2.sql
@@ -0,0 +1,84 @@
+-- 1/ Quels sont les tickets qui comportent l'article d'ID 500 ?
+-- Afficher le numéro de ticket uniquement.
+
+SELECT id_ticket
+FROM ticket
+    JOIN sale USING (id_ticket)
+    JOIN article USING (id_article)
+WHERE id_article = 500;
+
+-- 2/ Quels sont les tickets du 15/01/2017 ?
+-- Afficher le numéro de ticket et la date.
+
+
+-- 3/ Quels sont les tickets émis du 15/01/2017 au 17/01/2017 ?
+-- Afficher le numéro de ticket et la date.
+
+
+-- 4/ Quels sont les articles (Code et nom uniquement) apparaissant sur un ticket à au moins 95 exemplaires.
+
+
+-- 5/ Quels sont les tickets émis au mois de mars 2017 ?
+-- Afficher le numéro de ticket et la date.
+
+
+-- 6/ Quels sont les tickets émis au deuxième trimestre 2017 ?
+-- Afficher le numéro de ticket et la date.
+
+
+-- 7/ Quels sont les tickets émis au mois de mars et juillet 2017 ?
+-- Afficher le numéro de ticket et la date.
+
+
+-- 8/ Afficher la liste de toutes les bières classée par couleur.
+-- Afficher code et nom de bière, nom de la couleur
+
+
+-- 9/ Afficher la liste des bières n'ayant pas de couleur. 
+-- Afficher le code et le nom
+
+
+
+-- 10/ Lister pour chaque ticket la quantité totale d'articles vendus (en nombre).
+-- Classer par quantité décroissante
+
+
+
+-- 11/ Lister chaque ticket pour lequel la quantité totale d'articles vendus est inférieure à 50.
+-- Classer par quantité croissante
+
+
+
+-- 12/ Lister chaque ticket pour lequel la quantité totale d'articles vendus est supérieure à 500.
+-- Classer par quantité décroissante
+
+
+
+-- 13/ Lister chaque ticket pour lequel la quantité totale d'articles vendus est supérieure à 500. On exclura du total, les ventes de 50 articles et plus.
+-- classer par quantité décroissante
+
+
+
+-- 14/ Lister les bières de type ‘Trappiste'.
+-- Afficher id, nom de la bière, volume et titrage
+
+
+-- 15/ Lister les marques du continent ‘Afrique'.
+-- Afficher id et nom de marque, nom du continent
+
+
+-- 16/ Lister les bières du continent ‘Afrique'.
+-- Afficher ID, Nom et volume
+
+
+-- 17/ Lister les tickets classés par montant décroissant.
+-- Afficher l'année, numéro de ticket, montant total à payer
+--     En sachant que l'on applique un taux de TVA de 20% au montant total hors taxe de chaque ticket.
+
+
+-- 18/ Donner le C.A. par année.
+-- Afficher les années et Total HT
+
+
+-- 19/ Lister les quantités vendues de chaque article pour l'année 2017.
+-- Afficher id et nom de l'article, quantité vendue
