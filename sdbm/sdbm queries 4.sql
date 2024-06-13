fb03e6ba1b96a3b01c68b2df54a13cb72119502f
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-13 10:59:31 +0200

    queries 4 uploaded

---
+++ sdbm/sdbm queries 4.sql
@@ -0,0 +1,39 @@
+-- 1/ Récupérer toutes les bières les plus alcoolisés de chaque continent
+-- afficher le nom du contient , le nom de la bière, le degrès d'alcool et le volume 
+
+
+
+-- 2/ Récupérer le volume de bières vendu pour chaque mois et pour chaque type de bière
+-- classés par années, mois et type de bière
+
+
+-- 3/ Récupérer le nom et le volume des bières allemandes achetées en même temps que des bières françaises
+-- classés par nom de bière
+
+
+-- 4/ Récupérer la liste des bières pour lequelles les ventes ont agumentées entre 2015 et 2016
+
+
+
+-- 5/ Récupérer les bières pour lesquelles le volume de bières
+-- vendus est d'au moins 200 litres pour toutes les années
+
+
+
+-- 6/ Récupérer pour chaque pays la ou les marques de bière dont le degrès d'alcool moyen est le plus élevé en affichant le degré d'alcool moyen
+
+
+
+-- 7/ Donner pour chaque type de bière, la bière la plus vendue et la bière la moins vendue en 2016
+
+
+
+-- 8/ Donner pour toutes les couleurs de bières la plus vendue pour chacune des années 2015, 2016 et 2017 
+
+
+
+-- 9/ Lister les marques de bières dont le volume total vendu (en litres) en 2015 est supérieur à celui de Heineken.
+
+
+
+-- 10/ Lister les marques de bières dont le volume total vendu (en litres) est supérieur à celui de Heineken pour chaque année entre 2015 et 2017.
