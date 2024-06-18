c1371d7742418032600097c9358647caf55c3e5e
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-18 11:04:28 +0200

    uploaded queries 5 exercices file

---
+++ sdbm/sdbm queries 5.sql
@@ -0,0 +1,25 @@
+-- 1/ Donner la liste des fabriquants dont le type de bière les plus vendues en 2016 sont les abbayes.
+
+
+
+-- 2/ Automatiser le calcul de la quantité total de bière vendu en nombre de bière. pour un jour donné.
+
+
+
+-- 3/ Donner pour chaque année la ou les marques ayant vendues le plus gros volume de bière (en litres)
+
+
+
+-- 4/ Automatiser la mise à jour de la date d'un ticket à la date du jour à chaque ajout d'une bière à celui-ci.
+
+
+
+-- 5/ Donnez la liste des marques de bière dont au moins une bière a vendu plus de 500 unitées en 2016
+
+
+
+-- 6/ Automatiser le fait de pouvoir augmenter ou diminuer les prix de toutes les bières d'une même marque d'un certain pourcentage.
+
+
+
+-- 7/ Donnez pour chaque type de bière le pourcentage de répartition par continent (en nb d'article)
