77a57c6c6fce40af25e9182cc3632530548345f1
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-06-18 11:41:56 +0200

    Exercice 1 done

--- sdbm/sdbm queries 5.sql
+++ sdbm/sdbm queries 5.sql
@@ -1,6 +1,27 @@
 -- 1/ Donner la liste des fabriquants dont le type de bière les plus vendues en 2016 sont les abbayes.
 
+CREATE VIEW maker_beers_by_type AS
+SELECT YEAR(ticket_date) AS years, id_maker, maker_name, id_type, type_name, SUM(quantity) AS total_sold
+FROM maker
+    JOIN brand USING (id_maker)
+    JOIN article USING (id_brand)
+    JOIN type USING (id_type)
+    JOIN sale USING (id_article)
+    JOIN ticket USING (id_ticket)
+GROUP BY id_maker, id_type, years
+ORDER BY id_maker, id_type, years;
 
+
+SELECT years, id_maker, maker_name, id_type AS type_id, type_name, total_sold
+FROM maker_beers_by_type m
+WHERE total_sold IN (
+    SELECT total_sold
+    FROM maker_beers_by_type
+    WHERE id_type = m.id_type AND years = 2016
+)
+GROUP BY id_maker, total_sold, id_type, years
+HAVING type_name = 'Abbaye' AND years = 2016
+ORDER BY total_sold;
 
 -- 2/ Automatiser le calcul de la quantité total de bière vendu en nombre de bière. pour un jour donné.
 
